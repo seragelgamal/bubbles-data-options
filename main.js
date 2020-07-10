764bece63ef955800ac903477486771a3518f379
seragelgamal <55516965+seragelgamal@users.noreply.github.com>
2020-07-10 09:55:39 -0600

    Update main.js

--- main.js
+++ main.js
@@ -6,25 +6,59 @@ let ctx = cnv.getContext("2d");
 cnv.width = 800;
 cnv.height = 800;
 
-let bubbleInfo = [];
+let bubbles = [];
 
-for (let i = 0; i < 10; i++) {
-    bubbleInfo.push([randomDec(0, cnv.height), randomDec(0, cnv.width), randomDec(0, Math.max(cnv.height, cnv.width))]);
+document.addEventListener('keydown', keyDownHandler);
+
+function keyDownHandler(e) {
+    // console.log(e.key);
+    if (e.key == 'a') {
+        bubbles.push({
+            x: randomDec(0, cnv.width),
+            y: randomDec(0, cnv.height),
+            r: randomDec(0, Math.max(cnv.height, cnv.width))
+        });
+    } else if (e.key == 'p') {
+        bubbles.splice(randomInt(0, bubbles.length), 1);
+    } else if (e.key == 'r') {
+        bubbles.splice(0, bubbles.length);
+    }
 }
+
+document.addEventListener('click', clickHandler);
+
+function clickHandler(e) {
+    bubbles.push({
+        x: e.layerX,
+        y: e.layerY,
+        r: randomDec(0, Math.max(cnv.height, cnv.width))
+    });
+}
+
+// for (let i = 0; i < 10; i++) {
+//     bubbles.push({
+//         x: randomDec(0, cnv.width),
+//         y: randomDec(0, cnv.height),
+//         r: randomDec(0, Math.max(cnv.height, cnv.width))
+//     });
+// }
 
 // Main Program Loop
 requestAnimationFrame(draw);
 
 function draw() {
     ctx.clearRect(0, 0, cnv.width, cnv.height);
-    for (let i = 0; i < bubbleInfo.length; i++) {
+    for (let i = 0; i < bubbles.length; i++) {
         // Logic
-        for (let n = 0; n < bubbleInfo[i].length; n++) {
-            bubbleInfo[i][n] += randomDec(-5, 5);
-        }
+        bubbles[i].x += randomDec(-5, 5);
+        bubbles[i].y += randomDec(-5, 5);
+        bubbles[i].r += randomDec(-5, 5);
+        bubbles[i].x = constrain(bubbles[i].x, 0, cnv.width);
+        bubbles[i].y = constrain(bubbles[i].y, 0, cnv.height);
+        bubbles[i].r = constrain(bubbles[i].r, 0, Math.max(cnv.width, cnv.height));
 
         // drawing
-        strokeCircle(constrain(bubbleInfo[i][0], 0, cnv.width), constrain(bubbleInfo[i][1], 0, cnv.height), constrain(bubbleInfo[i][2], 0, Math.max(cnv.height, cnv.width)));
+        strokeCircle(bubbles[i].x, bubbles[i].y, bubbles[i].r);
     }
 
     // Request another Animation Frame
